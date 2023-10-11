da75173901d1c30338e4a37fb94c125da0dc4928
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-10-11 14:48:29 -0300

    adding override in withdraw method

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,37 +12,37 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);		
-		Account account;
 		
 		Scanner sc = new Scanner(System.in); 
 		
-		Account acc = new Account(1011, "Ana", 0.0);
-		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
-		
-		// UPCASTING
-		
-		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "Bobby", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Bia", 0.0, 0.01);
-		
-		//DOWNCAST
-		
-		BusinessAccount acc4 = (BusinessAccount)acc2;
-		acc4.loan(100.0);
+		Account acc = new Account(1011, "Ana", 1000.0);
+		acc.withdraw(200);
+		System.out.println(acc.getBalance());
+		Account acc2 = new SavingsAccount(1002, "Bia", 1000, 0.01);
+		acc2.withdraw(200);
+		System.out.println(acc2.getBalance());
+		/*
+		 * BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		 * 
+		 * // UPCASTING
+		 * 
+		 * Account acc1 = bacc; Account acc2 = new BusinessAccount(1003, "Bobby", 0.0,
+		 * 200.0); 	 
+		 * 
+		 * //DOWNCAST
+		 * 
+		 * BusinessAccount acc4 = (BusinessAccount)acc2; acc4.loan(100.0);
+		 */
 		
 		// BusinessAccount acc5 = (BusinessAccount)acc3; savings account to business account conversion is no accepted
 		
-		if (acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.0);
-			System.out.println("Loan!");
-		}
-		
-		if (acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount)acc3;
-			acc5.updateBalance();
-			System.out.println("Update!");
-		}
+		/*
+		 * if (acc3 instanceof BusinessAccount) { BusinessAccount acc5 =
+		 * (BusinessAccount)acc3; acc5.loan(200.0); System.out.println("Loan!"); }
+		 * 
+		 * if (acc3 instanceof SavingsAccount) { SavingsAccount acc5 =
+		 * (SavingsAccount)acc3; acc5.updateBalance(); System.out.println("Update!"); }
+		 */
 		
 		/* System.out.print("Enter account number: "); int number = sc.nextInt();
 		 * sc.nextLine(); System.out.print("Enter account holder: "); String holder =
