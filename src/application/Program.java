8536f865aa0d1050f352e000420db2c3047ff33b
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-10-12 19:54:03 -0300

    uncommenting code

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,52 +25,64 @@ public class Program {
 		acc3.withdraw(200);
 		System.out.println(acc3.getBalance());
 		
+		
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
 		/*
-		 * BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
-		 * 
 		 * // UPCASTING
 		 * 
-		 * Account acc1 = bacc; Account acc2 = new BusinessAccount(1003, "Bobby", 0.0,
-		 * 200.0); 	 
+		 * Account acc1 = bacc; Account acc4 = new BusinessAccount(1003, "Bobby", 0.0,
+		 * 200.0);
 		 * 
-		 * //DOWNCAST
+		 * // DOWNCAST
 		 * 
-		 * BusinessAccount acc4 = (BusinessAccount)acc2; acc4.loan(100.0);
+		 * BusinessAccount acc5 = (BusinessAccount) acc2; acc5.loan(100.0);
+		 * 
+		 * // BusinessAccount acc5 = (BusinessAccount)acc3; savings account to business
+		 * // account conversion is no accepted
+		 * 
+		 * if (acc3 instanceof BusinessAccount) { BusinessAccount acc6 =
+		 * (BusinessAccount) acc3; acc6.loan(200.0); System.out.println("Loan!"); }
+		 * 
+		 * if (acc3 instanceof SavingsAccount) { SavingsAccount acc5 = (SavingsAccount)
+		 * acc3; acc5.updateBalance(); System.out.println("Update!"); }
 		 */
-		
-		// BusinessAccount acc5 = (BusinessAccount)acc3; savings account to business account conversion is no accepted
-		
-		/*
-		 * if (acc3 instanceof BusinessAccount) { BusinessAccount acc5 =
-		 * (BusinessAccount)acc3; acc5.loan(200.0); System.out.println("Loan!"); }
-		 * 
-		 * if (acc3 instanceof SavingsAccount) { SavingsAccount acc5 =
-		 * (SavingsAccount)acc3; acc5.updateBalance(); System.out.println("Update!"); }
-		 */
-		
-		/* System.out.print("Enter account number: "); int number = sc.nextInt();
-		 * sc.nextLine(); System.out.print("Enter account holder: "); String holder =
-		 * sc.nextLine();
-		 * 
-		 * System.out.println("Is there an initial deposit (y/n)? "); char response =
-		 * sc.next().charAt(0);
-		 * 
-		 * if (response == 'y') { System.out.println("Enter initial deposit value: ");
-		 * double initialDeposit = sc.nextDouble(); account = new Account(number,
-		 * holder, initialDeposit); } else { account = new Account(number, holder); }
-		 * 
-		 * System.out.println(); System.out.println("Account data: ");
-		 * System.out.println(account); System.out.println();
-		 * 
-		 * System.out.print("Enter a deposit value: "); double depositValue =
-		 * sc.nextDouble(); account.deposit(depositValue);
-		 * System.out.println("Updated account data: "); System.out.println(account);
-		 * System.out.println();
-		 * 
-		 * System.out.print("Enter a withdraw value: "); double withdrawValue =
-		 * sc.nextDouble(); account.withdraw(withdrawValue);
-		 * System.out.println("Updated account data: "); System.out.println(account);
-		 */		
+		Account account;
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Enter account holder: ");
+		String holder = sc.nextLine();
+
+		System.out.println("Is there an initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+
+		if (response == 'y') {
+			System.out.println("Enter initial deposit value: ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);
+		} else {
+			account = new Account(number, holder);
+		}
+
+		System.out.println();
+		System.out.println("Account data: ");
+		System.out.println(account);
+		System.out.println();
+
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		System.out.println();
+
+		System.out.print("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		 		
 		
 		sc.close();
 
